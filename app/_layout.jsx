5e469f70b8a09cb0ace7d167dc317909a8d5deeb
sarvadandge29 <sarvadandge@gmail.com>
2025-01-25 20:08:06 +0530

    Home layout update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -31,6 +31,9 @@ const HomeLayout = () => {
     return (
         <Stack>
             <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+            <Stack.Screen name='(admin)' options={{ headerShown: false }} />
+            <Stack.Screen name='(root)' options={{ headerShown: false }} />
         </Stack>
     )
 }
